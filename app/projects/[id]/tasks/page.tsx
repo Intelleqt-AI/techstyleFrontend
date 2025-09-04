c3db6a1f9512c116de7e3e367172d774dfbdbbb8
Saif Hasan <saifhassan399@gmail.com>
2025-09-05 04:04:34 +0600

    Update - Projec task timeline view added

--- app/projects/[id]/tasks/page.tsx
+++ app/projects/[id]/tasks/page.tsx
@@ -314,6 +314,8 @@ export default function ProjectTasksPage({ params }: { params: { id: string } })
       toast.error('Failed to update task on server');
     }
   };
+  
+  console.log(tasks);
 
   return (
     <div className="flex-1 bg-gray-50 p-6">
